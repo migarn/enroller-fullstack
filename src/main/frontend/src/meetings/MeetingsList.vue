3440a1cd5f483cd4d92669c2607251cbf0413afd
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-27 17:27:23 +0200

    Empty table in MeetingsList

--- src/main/frontend/src/meetings/MeetingsList.vue
+++ src/main/frontend/src/meetings/MeetingsList.vue
@@ -1,9 +1,4 @@
 <template>
-
-	// do usunięcia
-	<div v-if="meetings.length > 0">Meetings.length = {{ meetings.length }}</div>
-	// do usunięcia
-
 <div>	
   <table v-if="meetings.length > 0"> 
     <thead>
@@ -15,67 +10,17 @@
     </tr>
     </thead>
     <tbody>
-    <tr v-for="meeting in meetings" :key="meeting.title">
-      <td>{{ meeting.title }}</td>
-      <td>{{ meeting.description }}</td>
-      <td>
-        <ul v-if="meeting.participants">
-          <li v-for="participant in meeting.participants" :key="participant">
-            {{ participant }}
-          </li>
-        </ul>
-      </td>
-      <td style="text-align: right; min-width: 400px">
-        <button v-if="meeting.participants.indexOf(username) < 0" class="button-outline"
-                @click="$emit('attend', meeting)">
-          Zapisz się
-        </button>
-        <button v-else class="button-outline" @click="$emit('unattend', meeting)">Wypisz się</button>
-        <button v-if="meeting.participants.length === 0" class="button" @click="$emit('delete', meeting)">
-          Usuń puste spotkanie
-        </button>
+    <tr>
+      <td>title</td>
+      <td>description</td>
+      <td>participants
+      </td>buttons
+
       </td>
     </tr>
     </tbody>
   </table>
-  </div>
-  
-  
-  
-  // do usunięcia
-  <div v-if="meetings.length > 0">
-    <table> 
-    <thead>
-    <tr>
-      <th>Nazwa spotkania</th>
-      <th>Opis</th>
-      <th>Uczestnicy</th>
-      <td></td>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>drzewo</td>
-      <td>jtyjty</td>
-      <td>
-        <ul>
-          <li>
-            jhtjtyjtyjtyjtyj
-          </li>
-        </ul>
-      </td>
-      <td style="text-align: right; min-width: 400px">
-        <button>
-          Zapisz się
-        </button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-  </div>
-  // do usunięcia
-  
-  
+  </div>  
 </template>
 
 <script>
